1f68b26eb860988fb7017b5c53fefd0f80526d67
Pedro Joaquim <pedro.cicoti.florencio@escola.pr.gov.br>
2025-02-24 08:21:33 -0300

    24-002

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  // Função draw significa desenhar
+  function draw() {
+    background(132,10,103);//escolha um código para a cor
+    
+    // Desenhando a linha superior
+    circle(52,53,100);// Primeiro círculo
+    circle(152,53,100);// Segundo círculo
+    circle(252,53,100);// Terceiro círculo
+    circle(352,53,100);// Quarto círculo
+    
+    // Desenhando a linha inferior
+    circle(52,348,100);// Primeiro círculo
+    circle(152,348,100);// Segundo círculo
+    circle(252,348,100);// Terceiro círculo
+    circle(352,348,100);// Quar
+  }
